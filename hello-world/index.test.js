4a305a31c7fb124f8b67a1da7dd2d4b1c5806ce1
Andreas Malmqvist <andilutten@pop-os.localdomain>
2020-12-01 15:16:07 +0100

    dont use babel

--- hello-world/index.test.js
+++ hello-world/index.test.js
@@ -1,5 +1,5 @@
-import puppeteer from 'puppeteer'
-import express from 'express'
+const puppeteer = require ('puppeteer')
+const express = require('express')
 
 const PORT = process.env.PORT || 8080
 
